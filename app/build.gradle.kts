5ec7bbdba16c69f2e24c52cdd9ec54f507b9ca22
osmanalisevinc <Sevinc32isparta@gmail.com>
2026-03-29 15:23:51 +0300

    firt lesson

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,7 +48,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
-    implementation(libs.androidx.core.splashscreen)
+    implementation(libs.androidx.core.splashscreen) // sonradan eklendi eğitimden
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
